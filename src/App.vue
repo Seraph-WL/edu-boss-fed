56c1ad5191c229aae8c33a4377821c10473f1ca8
Seraph-WL <1714479355@qq.com>
2021-11-29 08:17:14 +0800

    master

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="app">
-    <h1>拉勾教育</h1>
     <!-- 根路由出口，⽤于渲染路由组件 -->
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
 
 <style lang="scss"></style>
